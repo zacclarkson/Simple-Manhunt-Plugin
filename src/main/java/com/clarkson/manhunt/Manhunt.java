8266a26653a58adb69b83578c3d0a57159328186
zacclarkson <s4005654@student.rmit.edu.au>
2025-04-14 20:37:47 +1000

    Initialize project structure with .gitignore, pom.xml, main plugin class, and configuration file

---
+++ src/main/java/com/clarkson/manhunt/Manhunt.java
@@ -0,0 +1,17 @@
+package com.clarkson.manhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Manhunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
